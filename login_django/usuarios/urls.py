cf0ff11379eee297019031e91439717b986132f1
TestCI-LB <testtestci84@gmail.com>
2025-03-30 23:16:54 -0500

    Delete login_django directory

--- login_django/usuarios/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import login_view, bienvenida_view
-
-urlpatterns = [
-    path('', login_view, name='login'),
-    path('bienvenida/', bienvenida_view, name='bienvenida'),
-]
